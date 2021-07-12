eb16ad9eff8cfe724b94c564172159e4386f4745
Rodrigues da Silva <edurods@f8ffc23a9954.ant.amazon.com>
2021-07-12 13:55:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Serverless Circuit Breaker
+
+This project contains the implementation for a circuit breaker language agnostic solution made entirely using Serverless services.
---
+++ functions/make-payment-fallback.js
@@ -0,0 +1,29 @@
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.handler = async (event, context) => {
+    try {
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'Hello from fallback function'
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
